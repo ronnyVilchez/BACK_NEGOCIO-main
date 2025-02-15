3167cc1e4a0a0a5e209b663e1101709e8984cdf7
Ronny <ronnyd.1994@gmail.com>
2025-02-15 13:58:16 -0500

    thir

--- src/Routes/incident.routes.js
+++ src/Routes/incident.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { incidenUpdate, incidentAll, incidentCreate, incidentDelete, incidentFromUs, incidentId } from "../controllers/incident.controller.js";
+import { incidentAll, incidentCreate, incidentDelete, incidentFromUs, incidentId, incidentUpdate } from "../controllers/incident.controller.js";
 import {upload} from  "../config/multer.js"
 
 const router = Router()
@@ -8,7 +8,7 @@ router.get('/all',incidentAll)
 router.get('/:id',incidentId)
 router.get('/u/:id',incidentFromUs)
 router.post('/',upload.single('photo'), incidentCreate)
-router.patch('/:id',incidenUpdate)
+router.patch('/:id',incidentUpdate)
 router.delete('/:id',incidentDelete)
 
 export default router
